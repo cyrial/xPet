e9f8374cda98981cdc560e973dbee7e2da220faf
cyrial <koza.krzysztof@gmail.com>
2023-03-06 13:59:24 +0100

    Added animations
    Added animations for sleeping, shower, healing, reading, cake eating

--- objects.py
+++ objects.py
@@ -9,10 +9,15 @@ import utime
 
 POO = 0
 
-POO_SIZE = [24,24]
-FOOD_SIZE = [24,20]
-DRINK_SIZE = [18,18]
-BALL_SIZE = [23,23]
+POO_SIZE = [24, 24]
+FOOD_SIZE = [24, 20]
+CAKE_SIZE = [20, 18]
+DRINK_SIZE = [18, 18]
+BALL_SIZE = [23, 23]
+INJECTION_SIZE = [39, 20]
+BOOK_SIZE = [30, 17]
+SHOWER_SIZE = [22, 16]
+SLEEP_SIZE = [20, 18]
 
 def generate_poo():
   global POO
@@ -45,9 +50,24 @@ def clean_poo():
 def draw_food(step):
     tft.image(animations.ACTION_X + character.SIZE[0] + 5, animations.CH_Y + int(character.SIZE[1] / 2) - int(FOOD_SIZE[1] / 2), "images/food/" + str(step) + ".jpg")
 
+def draw_food(step):
+    tft.image(animations.ACTION_X + character.SIZE[0] + 5, animations.CH_Y + int(character.SIZE[1] / 2) - int(CAKE_SIZE[1] / 2), "images/cake/" + str(step) + ".jpg")
+    
 def draw_drink(step):
     tft.image(animations.ACTION_X + character.SIZE[0] + 5, animations.CH_Y + int(character.SIZE[1] / 2) - int(DRINK_SIZE[1] / 2), "images/drink/" + str(step) + ".jpg")
-  
+
+def draw_injection(step):
+    tft.image(animations.ACTION_X + character.SIZE[0] + 5, animations.CH_Y + int(character.SIZE[1] / 2) - int(INJECTION_SIZE[1] / 2), "images/injection/" + str(step) + ".jpg")
+    
+def draw_book(step):
+    tft.image(animations.ACTION_X + character.SIZE[0] + 5, animations.CH_Y + int(character.SIZE[1] / 2) - int(BOOK_SIZE[1] / 2), "images/book/" + str(step) + ".jpg")
+
+def draw_sleep(step):
+    tft.image(animations.ACTION_X + character.SIZE[0] + 5, animations.CH_Y + int(character.SIZE[1] / 5) - int(SLEEP_SIZE[1] / 2), "images/sleep/" + str(step) + ".jpg")
+    
+def draw_shower(step):
+    tft.image(animations.ACTION_X + int(character.SIZE[0]/2) - int(SHOWER_SIZE[0] / 2), animations.CH_Y - SHOWER_SIZE[1] - 5, "images/shower/" + str(step) + ".jpg")
+    
 def draw_ball(step, height, clear_height):
     tft.rect(animations.ACTION_X + character.SIZE[0] + 5, height-clear_height, BALL_SIZE[0], clear_height, tft.WHITE, tft.WHITE)
     tft.image(animations.ACTION_X + character.SIZE[0] + 5, height, "images/fun/" + str(step) + ".jpg")
