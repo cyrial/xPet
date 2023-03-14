05fd0953ea83238219f9773e9c85136a09f80579
cyrial <koza.krzysztof@gmail.com>
2023-03-14 23:00:07 +0100

    fixes

--- buttons.py
+++ buttons.py
@@ -1,5 +1,76 @@
-<<<<<<< HEAD
-from machine import Pinfrom sound_manager import play_soundimport stats_screenimport utimeimport gcimport sysimport _threadimport objectsimport animationsfrom stats_screen import increase_statSELECT_PIN = 21ENTER_PIN = 18EXIT_PIN = 19    def check_button():    while True:        # SELECT        if not select_handler.value():            play_sound('select')            stats_screen.select_from_menu()              # ENTER        if not enter_handler.value():            #play_sound('enter')                # CLEANING POO IF NOTHING IS SELECTED            if stats_screen.CURRENT_STAT == -1:                if objects.POO:                    objects.clean_poo()                        elif stats_screen.CURRENT_STAT == 0:                increase_stat("health")                animations.injection()                            elif stats_screen.CURRENT_STAT == 1:                increase_stat("drink")                animations.drinking()                            elif stats_screen.CURRENT_STAT == 2:                increase_stat("food")                animations.eating()                        elif stats_screen.CURRENT_STAT == 3:                increase_stat("sweets")                animations.eating()                            elif stats_screen.CURRENT_STAT == 4:                increase_stat("fun")                animations.fun()                         elif stats_screen.CURRENT_STAT == 5:                increase_stat("book")                animations.reading()                        elif stats_screen.CURRENT_STAT == 6:                increase_stat("wash")                animations.shower()                        elif stats_screen.CURRENT_STAT == 7:                # SLEEP MODE (start timer)                animations.sleeping()                            stats_screen.reset_highlight(1)                    # EXIT        if not exit_handler.value():            stats_screen.reset_highlight(1)            #play_sound('enter')                utime.sleep(0.05)    select_handler = Pin(SELECT_PIN, Pin.IN, Pin.PULL_UP)enter_handler = Pin(ENTER_PIN, Pin.IN, Pin.PULL_UP)exit_handler = Pin(EXIT_PIN, Pin.IN, Pin.PULL_UP)
-=======
-from machine import Pinfrom sound_manager import play_soundimport stats_screenimport utimeimport gcimport sysimport _threadimport objectsimport animationsfrom stats_screen import increase_statSELECT_PIN = 21ENTER_PIN = 18EXIT_PIN = 19    def check_button():    while True:        # SELECT        if not select_handler.value():            play_sound('select')            stats_screen.select_from_menu()              # ENTER        if not enter_handler.value():            #play_sound('enter')                # CLEANING POO IF NOTHING IS SELECTED            if stats_screen.CURRENT_STAT == -1:                if objects.POO:                    objects.clean_poo()                        elif stats_screen.CURRENT_STAT == 0:                increase_stat("health")                animations.injection()                            elif stats_screen.CURRENT_STAT == 1:                increase_stat("drink")                animations.drinking()                            elif stats_screen.CURRENT_STAT == 2:                increase_stat("food")                animations.eating()                        elif stats_screen.CURRENT_STAT == 3:                increase_stat("sweets")                animations.eating()                            elif stats_screen.CURRENT_STAT == 4:                increase_stat("fun")                animations.fun()                         elif stats_screen.CURRENT_STAT == 5:                increase_stat("book")                animations.reading()                        elif stats_screen.CURRENT_STAT == 6:                increase_stat("wash")                animations.shower()                        elif stats_screen.CURRENT_STAT == 7:                # SLEEP MODE (start timer)                animations.sleeping()                            stats_screen.reset_highlight(1)                    # EXIT        if not exit_handler.value():            stats_screen.reset_highlight(1)            #play_sound('enter')                utime.sleep(0.05)    select_handler = Pin(SELECT_PIN, Pin.IN, Pin.PULL_UP)enter_handler = Pin(ENTER_PIN, Pin.IN, Pin.PULL_UP)exit_handler = Pin(EXIT_PIN, Pin.IN, Pin.PULL_UP)
->>>>>>> parent of 82c0ddc... fixing new lines
+from machine import Pin
+from sound_manager import play_sound
+import stats_screen
+import utime
+import gc
+import sys
+import _thread
+import objects
+import animations
+from stats_screen import increase_stat
+
+SELECT_PIN = 21
+ENTER_PIN = 18
+EXIT_PIN = 19
+  
+  
+def check_button():
+    while True:
+        # SELECT
+        if not select_handler.value():
+            play_sound('select')
+            stats_screen.select_from_menu()
+      
+        # ENTER
+        if not enter_handler.value():
+            #play_sound('enter')
+    
+            # CLEANING POO IF NOTHING IS SELECTED
+            if stats_screen.CURRENT_STAT == -1:
+                if objects.POO:
+                    objects.clean_poo()
+            
+            elif stats_screen.CURRENT_STAT == 0:
+                increase_stat("health")
+                animations.injection()
+                
+            elif stats_screen.CURRENT_STAT == 1:
+                increase_stat("drink")
+                animations.drinking()
+                
+            elif stats_screen.CURRENT_STAT == 2:
+                increase_stat("food")
+                animations.eating()
+            
+            elif stats_screen.CURRENT_STAT == 3:
+                increase_stat("sweets")
+                animations.eating()
+                
+            elif stats_screen.CURRENT_STAT == 4:
+                increase_stat("fun")
+                animations.fun()
+             
+            elif stats_screen.CURRENT_STAT == 5:
+                increase_stat("book")
+                animations.reading()
+            
+            elif stats_screen.CURRENT_STAT == 6:
+                increase_stat("wash")
+                animations.shower()
+            
+            elif stats_screen.CURRENT_STAT == 7:
+                # SLEEP MODE (start timer)
+                animations.sleeping()
+                
+            stats_screen.reset_highlight(1)
+            
+        # EXIT
+        if not exit_handler.value():
+            stats_screen.reset_highlight(1)
+            #play_sound('enter')
+        
+        utime.sleep(0.05)
+    
+select_handler = Pin(SELECT_PIN, Pin.IN, Pin.PULL_UP)
+enter_handler = Pin(ENTER_PIN, Pin.IN, Pin.PULL_UP)
+exit_handler = Pin(EXIT_PIN, Pin.IN, Pin.PULL_UP)
--- sound_manager.py
+++ sound_manager.py
@@ -1,5 +1,42 @@
-<<<<<<< HEAD
-from machine import Pin, PWMfrom rtttl import RTTTLimport timeimport _threadSOUND_PIN = 25SOUNDS = [    'hatched:d=8,o=5,b=63:16b,16d6,b,16b,16d6,b,16b,16d6,4f6',    'alert:d=8,o=6,b=140:d,4f,d,d',    'success:d=4,o=5,b=63:32c,32e,32g,8b',    'poo:d=4,o=5,b=63:8b4,8g4,8e4,c4',    'select:d=8,o=6,b=140:16g',    'enter:d=8,o=6,b=140:16g5']def find_sound(name):    for sound in SOUNDS:        sound_name = sound.split(':')[0]        if sound_name == name:            return sound            def play_tone(freq, msec):    if freq > 0:    tone = PWM(Pin(SOUND_PIN, Pin.OUT), freq=int(freq), duty=90)    time.sleep(msec*0.001) # Play for a number of msec        tone.deinit()    pin = Pin(SOUND_PIN, Pin.OUT)    pin.value(1)    time.sleep(0.05) # Delay 50 ms between notes  def play_sound(snd):  tune = RTTTL(find_sound(snd))    for freq, msec in tune.notes():    play_tone(freq, msec)    def play_sound_thread(sound_name):    _thread.start_new_thread("SOUND_" + sound_name, play_sound, (sound_name,))    
-=======
-from machine import Pin, PWMfrom rtttl import RTTTLimport timeimport _threadSOUND_PIN = 25SOUNDS = [    'hatched:d=8,o=5,b=63:16b,16d6,b,16b,16d6,b,16b,16d6,4f6',    'alert:d=8,o=6,b=140:d,4f,d,d',    'success:d=4,o=5,b=63:32c,32e,32g,8b',    'poo:d=4,o=5,b=63:8b4,8g4,8e4,c4',    'select:d=8,o=6,b=140:16g',    'enter:d=8,o=6,b=140:16g5']def find_sound(name):    for sound in SOUNDS:        sound_name = sound.split(':')[0]        if sound_name == name:            return sound            def play_tone(freq, msec):    if freq > 0:    tone = PWM(Pin(SOUND_PIN, Pin.OUT), freq=int(freq), duty=90)    time.sleep(msec*0.001) # Play for a number of msec        tone.deinit()    pin = Pin(SOUND_PIN, Pin.OUT)    pin.value(1)    time.sleep(0.05) # Delay 50 ms between notes  def play_sound(snd):  tune = RTTTL(find_sound(snd))    for freq, msec in tune.notes():    play_tone(freq, msec)    def play_sound_thread(sound_name):    _thread.start_new_thread("SOUND_" + sound_name, play_sound, (sound_name,))      
->>>>>>> parent of 82c0ddc... fixing new lines
+from machine import Pin, PWM
+from rtttl import RTTTL
+import time
+import _thread
+
+SOUND_PIN = 25
+
+SOUNDS = [
+    'hatched:d=8,o=5,b=63:16b,16d6,b,16b,16d6,b,16b,16d6,4f6',
+    'alert:d=8,o=6,b=140:d,4f,d,d',
+    'success:d=4,o=5,b=63:32c,32e,32g,8b',
+    'poo:d=4,o=5,b=63:8b4,8g4,8e4,c4',
+    'select:d=8,o=6,b=140:16g',
+    'enter:d=8,o=6,b=140:16g5'
+]
+
+
+def find_sound(name):
+    for sound in SOUNDS:
+        sound_name = sound.split(':')[0]
+        if sound_name == name:
+            return sound
+            
+def play_tone(freq, msec):
+  
+  if freq > 0:
+    tone = PWM(Pin(SOUND_PIN, Pin.OUT), freq=int(freq), duty=90)
+    time.sleep(msec*0.001) # Play for a number of msec
+    
+    tone.deinit()
+    pin = Pin(SOUND_PIN, Pin.OUT)
+    pin.value(1)
+    time.sleep(0.05) # Delay 50 ms between notes
+  
+def play_sound(snd):
+  tune = RTTTL(find_sound(snd))
+  
+  for freq, msec in tune.notes():
+    play_tone(freq, msec)
+    
+def play_sound_thread(sound_name):
+    _thread.start_new_thread("SOUND_" + sound_name, play_sound, (sound_name,))
